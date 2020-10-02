874de9328a8b1989f8682582d7625b0c47d71fa5
taey1344 <xodus1344@gmail.com>
2020-10-03 01:51:49 +0900

    Login Router 만들기!

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 5000;
 
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const { User } = require('./models/User');
 
@@ -12,6 +13,9 @@ const { User } = require('./models/User');
 app.use(bodyParser.urlencoded({ extended: true }));
 // application/json 형태 정보 분석
 app.use(bodyParser.json());
+
+app.use(cookieParser());
+
 
 // mongoDB 연결
 const mongoose = require('mongoose');
@@ -41,6 +45,45 @@ app.post('/register', (req, res) => {
 
 })
 
+// Login 라우터
+app.post('/login', (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
 
+    // 요청된 이메일이 db에 있다면 비밀번호가 맞는지 확인한다.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+
+
+      // 비밀번호까지 맞을 경우, 토큰 생성한다.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 (쿠키, 로컬스토리지, 세션 ...)에 저장
+        // 여기선 "x_auth"라는 이름의 쿠키에 저장한다.
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+
+
+      })
+
+    })
+
+  })
+
+
+
+
+
+
+
+})
 
 app.listen(port, () => console.log(`Server on port ${port}!!`));
--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -15,7 +16,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    maxlength: 5
+    minlength: 5
   },
   lastname: {
     type: String,
@@ -54,6 +55,31 @@ userSchema.pre('save', function (next) {
     next()
   }
 });
+
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // plainPassword : 1234 랑 암호화된 비번 비교
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch)
+  })
+
+}
+
+userSchema.methods.generateToken = function (cb) {
+  let user = this;
+  //jsonwebtoken 으로 토큰 생성하기
+  // 여기서 _id는 db에 저장된 id 값
+  // user._id + 'secretToken' = token
+  // user._id.toHexString() --> plain object 자료형으로 바꿔줌?
+  let token = jwt.sign(user._id.toHexString(), 'secretToken')
+  user.toekn = token
+  user.save((err, user) => {
+    if (err) return cb(err);
+    cb(null, user)
+  })
+
+}
 
 // 스키마를 모델로 감싸줌
 const User = mongoose.model('User', userSchema);
