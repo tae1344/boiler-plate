75ba8d31ab7d3b4cbff7aa0b6ca9137997d45d5f
taey1344 <xodus1344@gmail.com>
2020-10-08 19:52:03 +0900

    Redux 사용 및 크롬 툴 연결

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+// 그냥 스토어는 객체만 받기 때문에, 프로미스와 함수도 받게 하도록 미들웨어와 함께 만들어주는 것이다.
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// 크롬 리덕스 개발 도구 설정 하는 구문
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+
+    )}
+  >
+
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
